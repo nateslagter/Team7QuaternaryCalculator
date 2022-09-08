1a8f1de4ee01899077f154e2ce23885785a01ea7
pfled <connor.pflederer@gmail.com>
2022-09-08 11:14:04 -0400

    Add math operation functions in model

--- src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
+++ src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
@@ -7,4 +7,50 @@ public class CalculatorModel {
     public String convertBaseFourToTen(String baseFourInt){
         return Integer.toString(Integer.parseInt(baseFourInt, 4), 10);
     }
+
+    public String add(String inputOne, String inputTwo){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        int operandTwo = Integer.parseInt(convertBaseFourToTen(inputTwo));
+        int result = operandOne + operandTwo;
+        String resultString = convertBaseTenToFour(Integer.toString(result));
+        return resultString;
+    }
+
+    public String subtract(String inputOne, String inputTwo){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        int operandTwo = Integer.parseInt(convertBaseFourToTen(inputTwo));
+        int result = operandOne - operandTwo;
+        String resultString = convertBaseTenToFour(Integer.toString(result));
+        return resultString;
+    }
+
+    public String multiply(String inputOne, String inputTwo){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        int operandTwo = Integer.parseInt(convertBaseFourToTen(inputTwo));
+        int result = operandOne * operandTwo;
+        String resultString = convertBaseTenToFour(Integer.toString(result));
+        return resultString;
+    }
+
+    public String divide(String inputOne, String inputTwo){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        int operandTwo = Integer.parseInt(convertBaseFourToTen(inputTwo));
+        int result = operandOne / operandTwo;
+        String resultString = convertBaseTenToFour(Integer.toString(result));
+        return resultString;
+    }
+
+    public String square(String inputOne){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        int result = operandOne * operandOne;
+        String resultString = convertBaseTenToFour(Integer.toString(result));
+        return resultString;
+    }
+
+    public String root(String inputOne){
+        int operandOne = Integer.parseInt(convertBaseFourToTen(inputOne));
+        double result = Math.sqrt(operandOne);
+        String resultString = convertBaseTenToFour(Integer.toString((int)(result)));
+        return resultString;
+    }
 }
