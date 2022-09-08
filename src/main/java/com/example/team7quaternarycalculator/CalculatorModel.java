efe36451d870e4478c3f8c33d71baeaf3ef67e72
Nate Slagter <nateslagter@gmail.com>
2022-09-08 11:41:04 -0400

    Add tests for conversion logic

--- src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
+++ src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
@@ -4,6 +4,7 @@ public class CalculatorModel {
     public String convertBaseTenToFour(String baseTenInt){
         return Integer.toString(Integer.parseInt(baseTenInt, 10), 4);
     }
+
     public String convertBaseFourToTen(String baseFourInt){
         return Integer.toString(Integer.parseInt(baseFourInt, 4), 10);
     }
--- src/test/java/com/example/team7quaternarycalculatortest/CalculatorModelTest.java
+++ src/test/java/com/example/team7quaternarycalculatortest/CalculatorModelTest.java
@@ -1,4 +1,26 @@
 package com.example.team7quaternarycalculatortest;
 
+import com.example.team7quaternarycalculator.CalculatorModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorModelTest {
+
+    CalculatorModel calculatorModel = new CalculatorModel();
+
+    @Test
+    public void TestBase4ToBase10()
+    {
+        String convertedNum = calculatorModel.convertBaseTenToFour("10");
+
+        Assertions.assertEquals("22", convertedNum);
+    }
+
+    @Test
+    public void TestBase10ToBase4()
+    {
+        String convertedNum = calculatorModel.convertBaseFourToTen("321032");
+
+        Assertions.assertEquals("3662",convertedNum);
+    }
 }
