179794eec150254bdddc4b9544af5de40e51f22f
MarsWillCode <ng526p@gmail.com>
2022-09-08 10:19:19 -0400

    Create convertors for baseTen and baseFour

--- src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
+++ src/main/java/com/example/team7quaternarycalculator/CalculatorModel.java
@@ -1,4 +1,10 @@
 package com.example.team7quaternarycalculator;
 
 public class CalculatorModel {
+    public String convertBaseTenToFour(String baseTenInt){
+        return Integer.toString(Integer.parseInt(baseTenInt, 10), 4);
+    }
+    public String convertBaseFourToTen(String baseFourInt){
+        return Integer.toString(Integer.parseInt(baseFourInt, 4), 10);
+    }
 }
