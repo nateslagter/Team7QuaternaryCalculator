88ee0233da799f81137361f6a33b7e853825ee42
peschaefer <peschaefer@bsu.edu>
2022-09-09 09:45:12 -0400

    Adjust Calculator styling
    Made the window a little larger, and updated the buttons to be easier to read and less congested.  Also updated colors including text color, button color, and window color.
    
    Updated convert button text.  The two arrows were difficult to read and make the button feel cluttered.

--- src/main/java/com/example/team7quaternarycalculator/CalculatorApplication.java
+++ src/main/java/com/example/team7quaternarycalculator/CalculatorApplication.java
@@ -11,9 +11,10 @@ public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 200, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 200, 350);
         stage.setTitle("Calculator");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
